d5a564ef8c84b854eeee7fe12ce9c7cf19dfb801
bartosz121 <bmagiera121@gmail.com>
2022-08-17 19:02:50 +0200

    Add stores.js

---
+++ src/stores.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+import { writable, readable } from "svelte/store";
+
+async function fetchSensorData() {
+  loading.set(true)
+  const res = await axios.get("https://piec.bartoszmagiera.me/data")
+  const data = res.data;
+  loading.set(false)
+  return data;
+}
+
+export const chartId = writable("mainChart");
+export const loading = writable(false);
+
+export const sensorData = readable(await fetchSensorData(), function start(set) {
+  const interval = setInterval(async () => {
+    set(await fetchSensorData())
+  }, 180000) // update every 3 min TODO make this time frame a state and store it in local storage
+
+  return function stop() {
+    clearInterval(interval)
+  }
+})
