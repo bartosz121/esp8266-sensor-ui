717055003cc12c2c5c4088c61336f1fb57601d8b
bartosz121 <bmagiera121@gmail.com>
2022-08-17 22:25:00 +0200

    Add getBrowserLocale and dateToFormattedString functions

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,21 @@
 import axios from "axios";
 
-import { loading } from "./stores.js";
+import { loading, userBrowserLocale } from "./stores.js";
 
+const localeStringOptions = { weekday: 'long', day: 'numeric', month: "short", hour: "2-digit", minute: "2-digit", second: "2-digit", };
+
+
+export function getBrowserLocale() {
+  if (navigator.languages) {
+    if (navigator.languages.length) {
+      return navigator.languages[0]
+    } else {
+      return navigator.language
+    }
+  } else {
+    return "en-US"
+  }
+}
 /**
  * @param {string} timestamp
  */
@@ -10,4 +24,13 @@ export function toLocalTimezone(timestamp) {
     new Date(timestamp).getTime() - new Date().getTimezoneOffset() * 60000
   );
   return dt;
+}
+
+/**
+ * @param {string} userLocale
+ * @param {Date} dt
+ */
+export function dateToFormattedString(userLocale, dt) {
+  // @ts-ignore
+  return dt.toLocaleDateString(userLocale, localeStringOptions)
 }
