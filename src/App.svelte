e56abe3a538c2f8e2a43c1c21d1dde85f42ebf0f
bartosz121 <bmagiera121@gmail.com>
2022-08-17 19:04:22 +0200

    Add App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,31 @@
 <script>
-  import "./app.css"
-  import TempChart from './lib/TempChart.svelte'
+  import { fade } from "svelte/transition";
+  import Chart from "./lib/Chart.svelte";
+  import Spinner from "./lib/Spinner.svelte";
+  import "./app.css";
+
+  import { loading } from "./stores.js";
 </script>
 
-<main>
-    <TempChart />
+<main class="h-screen w-screen">
+  {#if $loading}
+    <div
+      out:fade={{ duration: 1000 }}
+      class="absolute h-full w-full flex justify-center items-center"
+    >
+      <Spinner />
+    </div>
+  {/if}
+  <Chart />
+
+  <!-- <button
+      on:click={() =>
+        ApexCharts.exec(
+          "mainChart",
+          "zoomX",
+          toLocalTimezone("2022-08-12 18:00:00").getTime(),
+          toLocalTimezone("2022-08-12 20:00:00").getTime()
+        )}
+      class="p-4 bg-pink-400 rounded">click me</button
+    > -->
 </main>
